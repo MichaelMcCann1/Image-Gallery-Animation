b3d8736d12dd44ea7394fafc8453a5eb732065c5
Michael McCann <81657333+MichaelMcCann1@users.noreply.github.com>
2021-09-20 15:00:58 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './App.css';
+import { IoMdArrowDropupCircle, IoMdArrowDropdownCircle } from "react-icons/io"
+
+function App() {
+
+  const [top, setTop] = useState(50)
+  const imageArray = [{src:'house1.jpg', alt:""}, {src:'house2.jpg', alt:""},{src:'house3.jpg', alt:""},{src:'house4.jpg', alt:""},{src:'house5.jpg', alt:""}]
+
+  const moveUp = () => {
+    if (top === 50) return
+    setTop(t => t + 100)
+  }
+
+  const moveDown = () => {
+    if (top === -100*(imageArray.length - 1)+50) return
+    setTop(t => t - 100)
+  }
+
+  return (
+    <div className="gallery">
+      <div className="left">
+        {imageArray.map((item, index) => (
+          <img key={index} style={{top: `${index*100+top}%`}} src={`Images/${item.src}`} alt={item.alt}></img>
+        ))}
+      </div>
+      <div className="right">
+        {imageArray.map((item, index) => (
+          <img key={index} style={{top: `${-index*100-top+100}%`}} src={`Images/${item.src}`} alt={item.alt}></img>
+        ))}
+      </div>
+      <div className="upArrow" onClick={moveUp}>
+        <IoMdArrowDropupCircle/>
+      </div>
+      <div className="downArrow" onClick={moveDown}>
+        <IoMdArrowDropdownCircle/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
